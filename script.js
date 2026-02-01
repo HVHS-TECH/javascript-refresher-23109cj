8eefc229c7d13c24d729bdcd7ff5cb3c9d7973fc
23109cj <23109cj@hvhs.school.nz>
2026-02-01 22:03:09 +0000

    test123

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Congratulations!\nYou got the script running.")
+
+
+document.getElementById("test").onsubmit = function(event) {
+  event.preventDefault();
+  alertUser();
+};
+
+function alertUser() {
+    alert("Congratulations!\nYou got the script running.");
+}
