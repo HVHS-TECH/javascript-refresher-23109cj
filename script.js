fcf3ec27f1c7e2094676cbd36b47442c1464c3cd
23109cj <23109cj@hvhs.school.nz>
2026-02-03 12:23:34 +1300

    Done on the extension stuff

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-alert("test")
-
-
 //testing
 function alertUser() {
     alert("Congratulations!\nYou got the script running.");
 }
 
-let element = null
-var textInput = null
+let element = null;
+var textInput = null;
 function userInput(){
-  textInput = i_text.value.trim()
-  element = document.getElementById("welcomeMessage")
+  textInput = i_text.value.trim();
+  element = document.getElementById("welcomeMessage");
+  console.log(element);
+  element.innerHTML = textInput;
 }
