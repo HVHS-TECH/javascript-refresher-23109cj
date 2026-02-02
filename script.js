51526c1a56a2d32faa7e484937813b9b10459b34
23109cj <23109cj@hvhs.school.nz>
2026-02-03 12:14:20 +1300

    working

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ alert("test")
 function alertUser() {
     alert("Congratulations!\nYou got the script running.");
 }
+
+let element = null
+var textInput = null
+function userInput(){
+  textInput = i_text.value.trim()
+  element = document.getElementById("welcomeMessage")
+}
